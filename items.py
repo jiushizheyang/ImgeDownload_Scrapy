b7c04b164de5bca902fa19ef4b5e95110db86a3c
jiushizheyang <867191827@qq.com>
2017-07-18 21:36:57 +0800

    Add files via upload

---
+++ items.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class XiaoHuaItem(scrapy.Item):
+    image_urls = scrapy.Field()
+    text = scrapy.Field()
+    pages = scrapy.Field()
+    tags = scrapy.Field()
+    pre_tags = scrapy.Field()
